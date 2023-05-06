f734c4e884adb48e89ffc994fe245fcb48f48f74
Nischay Chauhan <masternischay@outlook.com>
2023-05-06 10:10:13 +0530

    Initial

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts" , (req , res)=>{
+    res.status(200).json({message : "Get all the contacts"})
+    console.log("Welcome to the contacts")
+})
+
+app.listen(port , ()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+})
